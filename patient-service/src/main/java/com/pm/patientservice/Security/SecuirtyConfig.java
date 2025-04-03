ee44269daf0de357f9ce11a74a114b122db43e15
Prashan070 <prashansrivastava77@gmail.com>
2025-04-03 23:36:57 +0530

    basic auth

--- patient-service/src/main/java/com/pm/patientservice/Security/SecuirtyConfig.java
+++ patient-service/src/main/java/com/pm/patientservice/Security/SecuirtyConfig.java
@@ -1,5 +1,56 @@
 package com.pm.patientservice.Security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecuirtyConfig {
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).
+                authorizeHttpRequests(request -> {
+                    request.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+                    request.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("Admin")
+                .build();
+
+        UserDetails normalPublic = User.builder()
+                .username("normalPublic")
+                .password(passwordEncoder().encode("normalPublic"))
+                .roles("NormalPublic")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,normalPublic);
+    }
+
 }
